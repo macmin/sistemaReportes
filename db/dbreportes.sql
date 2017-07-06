3fc0784cc33d7f7481a9e40844084492fefe02e0
macmin9998 <mac10unam@gmail.com>
2017-07-06 16:29:15 -0500

    base de datos correcta

---
+++ db/dbreportes.sql
@@ -0,0 +1,277 @@
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `movimientos`
+--
+
+DROP TABLE IF EXISTS `movimientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movimientos` (
+  `movimientoId` int(11) NOT NULL AUTO_INCREMENT,
+  `productoId` int(11) DEFAULT NULL,
+  `tipoMovimiento` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `dateInsert` date DEFAULT NULL,
+  `userInsert` int(11) DEFAULT NULL,
+  PRIMARY KEY (`movimientoId`),
+  KEY `userInsert` (`userInsert`),
+  KEY `productoId` (`productoId`),
+  KEY `tipoMovimiento` (`tipoMovimiento`),
+  CONSTRAINT `movimientos_ibfk_1` FOREIGN KEY (`productoId`) REFERENCES `productos` (`productoId`),
+  CONSTRAINT `movimientos_ibfk_2` FOREIGN KEY (`tipoMovimiento`) REFERENCES `tipomovimiento` (`tipoId`),
+  CONSTRAINT `movimientos_ibfk_3` FOREIGN KEY (`userInsert`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimientos`
+--
+
+LOCK TABLES `movimientos` WRITE;
+/*!40000 ALTER TABLE `movimientos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimientos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `procesos`
+--
+
+DROP TABLE IF EXISTS `procesos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `procesos` (
+  `procesoId` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreProceso` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`procesoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `procesos`
+--
+
+LOCK TABLES `procesos` WRITE;
+/*!40000 ALTER TABLE `procesos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `procesos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productos` (
+  `productoId` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `ean` varchar(100) DEFAULT NULL,
+  `codigoAlt` varchar(100) DEFAULT NULL,
+  `dateInsert` date DEFAULT NULL,
+  `userInsert` int(11) DEFAULT NULL,
+  PRIMARY KEY (`productoId`),
+  KEY `userInsert` (`userInsert`),
+  KEY `nombre` (`nombre`),
+  KEY `ean` (`ean`),
+  CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`userInsert`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reporte`
+--
+
+DROP TABLE IF EXISTS `reporte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reporte` (
+  `reporteId` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `datosProducto` blob,
+  `dateInsert` date DEFAULT NULL,
+  `userInsert` int(11) DEFAULT NULL,
+  PRIMARY KEY (`reporteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reporte`
+--
+
+LOCK TABLES `reporte` WRITE;
+/*!40000 ALTER TABLE `reporte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reporte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rol_proceso`
+--
+
+DROP TABLE IF EXISTS `rol_proceso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rol_proceso` (
+  `rolId` int(11) DEFAULT NULL,
+  `procesoId` int(11) DEFAULT NULL,
+  KEY `rolId` (`rolId`),
+  KEY `procesoId` (`procesoId`),
+  CONSTRAINT `rol_proceso_ibfk_1` FOREIGN KEY (`rolId`) REFERENCES `roles` (`rolId`),
+  CONSTRAINT `rol_proceso_ibfk_2` FOREIGN KEY (`procesoId`) REFERENCES `procesos` (`procesoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol_proceso`
+--
+
+LOCK TABLES `rol_proceso` WRITE;
+/*!40000 ALTER TABLE `rol_proceso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rol_proceso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `rolId` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`rolId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipomovimiento`
+--
+
+DROP TABLE IF EXISTS `tipomovimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipomovimiento` (
+  `tipoId` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `dateInsert` date DEFAULT NULL,
+  `userInsert` int(11) DEFAULT NULL,
+  PRIMARY KEY (`tipoId`),
+  KEY `userInsert` (`userInsert`),
+  CONSTRAINT `tipomovimiento_ibfk_1` FOREIGN KEY (`userInsert`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipomovimiento`
+--
+
+LOCK TABLES `tipomovimiento` WRITE;
+/*!40000 ALTER TABLE `tipomovimiento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipomovimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_log`
+--
+
+DROP TABLE IF EXISTS `user_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_log` (
+  `logId` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `proceso` varchar(100) DEFAULT NULL,
+  `detalle` varchar(100) DEFAULT NULL,
+  `dateLog` date DEFAULT NULL,
+  PRIMARY KEY (`logId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_log`
+--
+
+LOCK TABLES `user_log` WRITE;
+/*!40000 ALTER TABLE `user_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `app` varchar(100) DEFAULT NULL,
+  `apm` varchar(100) DEFAULT NULL,
+  `dateInsert` date DEFAULT NULL,
+  `rolId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  KEY `rolId` (`rolId`),
+  KEY `username` (`username`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rolId`) REFERENCES `roles` (`rolId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-06 16:28:24
