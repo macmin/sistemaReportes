6df8ec83bd3b804c050de61f64aaae9ddebeda06
carlos117 <jose-ccr93@hotmail.com>
2017-07-07 09:48:05 -0500

    opciones de usuario

---
+++ opcionUsuario.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Usuarios</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+		<link rel="stylesheet" href="css/estilos.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+	</head>
+	<body>
+		<header>
+			<h1>Opciones para Usuarios</h1>
+		</header>
+			<form class="contenedor">
+				<center>
+					<button class="boton"><a href="usuarios.php">Adicionar Usario</a></button>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<button class="boton"><a href="modificarUsuario.php">Modificar Usario</a></button>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<button class="boton"><a href="menuAdministrador.php">Regresar</a></button>
+				</center>
+			</form>
+	</body>
+</html>
