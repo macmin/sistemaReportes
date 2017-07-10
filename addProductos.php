4398a06ed488a2cfd1cc20411001cb75b8bb4e74
carlos117 <jose-ccr93@hotmail.com>
2017-07-10 11:21:44 +0200

    menu addProductos modificado

--- addProductos.php
+++ addProductos.php
@@ -1,9 +1,17 @@
+<?php
+
+	session_start();
+    
+    if( isset( $_SESSION['name'] ) and isset( $_SESSION['userId'] ) ) {
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Adicionar Producto</title>
 		<link rel="stylesheet" type="text/css" href="css/estilos.css">
 		<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+		<link rel="stylesheet" href="css/flexboxgrid.min.css">
 
 		<script src="js/link.js"></script>
 
@@ -12,22 +20,41 @@
 	<header>
 		<h1>Adicionar Producto</h1>
 	</header>
-	<form class="contenedor">
-		<h2>EAN:</h2>
-		<input type="text" class="cajaTexto" id="txtEan">
-		<h2>Nombre:</h2> 
-		<input type="text" class="cajaTexto" name="txtNombre" >
-		<h2>Descripcion:</h2>
-		<input type="text" class="cajaTexto" name="txtDesc" >
-		<h2>Codigo Alterno:</h2>
-		<input type="text" class="cajaTexto" id="txtcodAlt">	
 
-		<button class="boton" id="btnRegistrar">Registrar</button>
+	<input type="hidden" id="hiddenId" value="<?php echo $_SESSION['userId'];?>">
+	<div class="contenedorProductos">
+		<div class="row around-xs center-xs">
+    		<div class="col-xs-6">
+				<h2>EAN:</h2>
+				<input type="text" class="cajaTexto" id="txtEan">
+			</div>
+			<div class="col-xs-6">
+				<h2>Codigo Alterno:</h2>
+				<input type="text" class="cajaTexto" id="txtcodAlt">
+			</div>
+		</div>
+
+		<div class="row around-xs center-xs">
+    		<div class="col-xs-6">
+				<h2>Nombre:</h2> 
+				<input type="text" class="cajaTexto" id="txtNombre" >
+			</div>
+			<div class="col-xs-6">
+				<h2>Descripcion:</h2>
+				<input type="text" class="cajaTexto" id="txtDesc" >
+			</div>
+		</div>
 		<br>
 		<br>
-		<button class="boton" type="button" onclick="javascript:productos();">Regresar</button>
-
-	</form>
+		<div class="row around-xs center-xs">
+    		<div class="col-xs-6">
+				<button class="boton" type="button" onclick="javascript:productos();">Regresar</button>
+			</div>
+			<div class="col-xs-6">
+				<button class="boton" id="btnRegistrar">Registrar</button>	
+			</div>
+		<div>
+	</div>
 
 
 	<script >
@@ -41,15 +68,17 @@
 				var cajaNombre = $("#txtNombre").val();
 				var cajaDescripcion = $("#txtDesc").val();
 				var cajaCodAlt = $("#txtcodAlt").val();
+				var cajaUserId = $("#hiddenId").val();
 
 				
-				$.post('/ws/wsProductos.PHP',
+				$.post('ws/wsProductos.php',
 					{
 						WS:"addProducto",
 						ean: cajaEan,
 						nombre:cajaNombre,
 						descripcion: cajaDescripcion,
-						codAlt:cajaCodAlt
+						codAlt:cajaCodAlt,
+						userId:cajaUserId
 						
 
 
@@ -68,4 +97,14 @@
 	</script>
 
 	</body>
+
+<?php
+	
+	}else{
+		echo "no hay sesion";
+
+	}
+
+?>
+
 </html>
